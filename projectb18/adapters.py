dc9b41c5053bb733f624e974192c77c7fa2f66b7
rythama <rythama@gmail.com>
2025-02-16 21:24:48 -0500

    Updating adapters to force skip extra info

--- projectb18/adapters.py
+++ projectb18/adapters.py
@@ -1,10 +1,15 @@
-# myapp/adapters.py
-
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_auto_signup_allowed(self, request, sociallogin):
         """
-        Return True to always skip the 'Sign Up' form and go straight to login.
+        Return True to skip the 'Sign Up' form entirely.
         """
         return True
+
+    def requires_additional_signup(self, request, sociallogin):
+        """
+        If this returns True, Allauth will show the sign-up form.
+        We want to skip it, so return False.
+        """
+        return False
