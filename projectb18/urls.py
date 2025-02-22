38e0fe91e2088e3c67855c15f2eb8a534dfe2537
AnthonyJia <anthonyjiang117@gmail.com>
2025-02-21 21:49:16 -0500

    deleted views.py in project folder and updated urls.py

--- projectb18/urls.py
+++ projectb18/urls.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from projectb18.views import landing, root_view
 
 urlpatterns = [
-    # The landing page for authenticated users
-    path('home/', landing, name='home'),
-
-    # Route all authentication URLs to the accounts app
-    path('accounts/', include('accounts.urls')),  
-
-    # Admin URLs
-    path('admin/', admin.site.urls),
-
-    # The root URL
-    path('', root_view, name='root'),
+   path('admin/', admin.site.urls),
+   path('', include('accounts.urls')),
 ]
