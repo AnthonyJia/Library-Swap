8dda771a463095e32147ac2cfd3e2219479eb2c7
rythama <rythama@gmail.com>
2025-02-22 00:07:04 -0500

    added google auth pages to login and signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import home  # Import the home view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),  # Default homepage
