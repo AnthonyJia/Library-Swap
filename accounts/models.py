8cc47b56d0a7c68c9d9fbed9866db89f5ebbfcda
rythama <rythama@gmail.com>
2025-02-22 20:14:58 -0500

    provider only allowed w/ admin permissions

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,18 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
-        ('provider', 'Provider'),
         ('borrower', 'Borrower'),
+        ('provider', 'Provider'),
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, blank=True, null=True)
+    def save(self, *args, **kwargs):
+        # If the user is approved but not currently a provider, set them to provider
+        if self.is_provider_approved and self.role != 'provider':
+            self.role = 'provider'
+        super().save(*args, **kwargs)
+
+    # New field to store admin approval for providers
+    is_provider_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
