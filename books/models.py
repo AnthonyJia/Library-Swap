2dddfb9a93aa6c0cf3072665a9f56a967f094aa1
AnthonyJia <anthonyjiang117@gmail.com>
2025-04-09 23:26:35 -0400

    added borrowrequest model to handle borrow_requests from users

--- books/models.py
+++ books/models.py
@@ -16,8 +16,6 @@ class Book(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='book_covers/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    borrowed_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL, related_name='borrowed_books')
-    borrowed_at = models.DateTimeField(null=True, blank=True)
 
     def save(self, *args, **kwargs):
         # Automatically set uploader_email if not already set and user exists.
@@ -29,7 +27,7 @@ class Book(models.Model):
         return self.title
     
     def is_available(self):
-        return self.borrowed_by is None
+        return not self.borrow_requests.filter(status='approved').exists()
     
 class Collection(models.Model):
     PRIVACY_CHOICES = [
@@ -61,4 +59,29 @@ class Collection(models.Model):
         return f"{self.title} ({self.visibility})"
 
 
+class BorrowRequest(models.Model):
+    REQUEST_STATUS = [
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+        ('returned', 'Returned'),
+    ]
 
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='borrow_requests')
+    requester = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='borrow_requests_list')
+    message = models.TextField(blank=True)
+    requested_at = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=10, choices=REQUEST_STATUS, default='pending')
+    approved_at = models.DateTimeField(null=True, blank=True)
+    responded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='handled_requests'
+    )
+
+    def __str__(self):
+        return f"Request by {self.requester} for '{self.book.title}' ({self.status})"
