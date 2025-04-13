2ef59ade53080e1892bf8e32b75fdd97f0eb2af3
rythama <rythama@gmail.com>
2025-04-13 17:27:38 -0400

    changed redis_url config

--- projectb18/settings.py
+++ projectb18/settings.py
@@ -47,7 +47,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get("REDIS_URL", "rediss://:p99ff3982e5577bc63a5024a93d29a96daaa6085863c65b819bcbc28d7ec2a8c9@ec2-52-21-156-131.compute-1.amazonaws.com:31350")],
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
         },
     },
 }
