3514beb3cc1a4d5af90a6937e5ac40b4d96285e2
SnehaMoothedan <nsd3tb@virginia.edu>
2025-04-14 17:09:55 -0400

    fixed URL namespace

--- chat/urls.py
+++ chat/urls.py
@@ -5,6 +5,6 @@ app_name = 'chat'  # Namespace for URL resolution
 
 urlpatterns = [
     path('', chat_view, name="chat_view"),
-    path('chat/<username>', get_or_create_chatroom, name = "start-chat"),
-    path("chat/room/<chatroom_name>", chat_view, name = "chatroom"),
+    path('<username>', get_or_create_chatroom, name = "start-chat"),
+    path("room/<chatroom_name>", chat_view, name = "chatroom"),
 ]
