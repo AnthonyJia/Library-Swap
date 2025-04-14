023f98687f1425dc431227b4209a07c9c103b8e4
rythama <rythama@gmail.com>
2025-04-14 18:16:21 -0400

    patrons no longer request provider

--- accounts/views.py
+++ accounts/views.py
@@ -33,7 +33,7 @@ def choose_view(request):
                 return redirect('provide_page')
             else:
                 messages.error(request, "You do not have permission to be a provider yet. Please request provider status below.")
-                return redirect('request_provider')  # Redirect to a view where they can request provider status.
+                #return redirect('request_provider')  # Redirect to a view where they can request provider status.
         elif choice == 'borrower':
             # They want to be a borrower
             request.user.role = 'borrower'
